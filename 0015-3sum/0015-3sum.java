7038849b80d02ade8a3654a65153c3a19aadd97e
Muskan Thakur <152734732+musk-web@users.noreply.github.com>
2024-12-31 14:12:35 +0530

    Time: 29 ms (71.84%), Space: 51.1 MB (91.16%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -39,24 +39,3 @@ class Solution {
         return ans;
     }
 }
-
-//     public static void main(String[] args) {
-//         int[] arr = { -1, 0, 1, 2, -1, -4};
-//         int n = arr.length;
-//         List<List<Integer>> ans = triplet(n, arr);
-//         for (List<Integer> it : ans) {
-//             System.out.print("[");
-//             for (Integer i : it) {
-//                 System.out.print(i + " ");
-//             }
-//             System.out.print("] ");
-//         }
-//         System.out.println();
-//     }
-// }
-
-
-
-      
-//     }
-// }
